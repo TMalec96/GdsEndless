35797880e0247d42246e7f5720dc4d81760830d1
TMalec96 <tomasz.malecki1996@gmail.com>
2020-02-06 23:42:15 +0100

    Basic dishes mechanism

--- EndlessTavern/Assets/Scripts/Dish.cs
+++ EndlessTavern/Assets/Scripts/Dish.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class Dish : MonoBehaviour
 {
-    public enum DishColors
-    {
-        White,
-        Red,
-        Yellow,
-        Blue,
-        Pink
-    }
-    [SerializeField]
-    DishColors dishColor = DishColors.Blue;
     // Start is called before the first frame update
     void Start()
     {
---
+++ EndlessTavern/Assets/Scripts/OrderDishSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderDishSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private List<Vector2> dishesPosition;
+    [SerializeField]
+    List<Dish> dishes;
+    void Start()
+    {
+        dishesPosition = setDishesPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        
+    }
+
+    private List<Vector2> setDishesPosition()
+    {
+        List<Vector2> tempList = new List<Vector2>();
+        for(int i = 0; i< 3; i++)
+        {
+            tempList.Add(gameObject.transform.GetChild(i).position);
+            print(gameObject.transform.GetChild(i).position);
+        }
+        return tempList;
+
+    }
+    private void allocateDishesPosition()
+    {
+        for (int i = 0;i< dishesPosition.Capacity; i++)
+        {
+
+        }
+    }
+}
