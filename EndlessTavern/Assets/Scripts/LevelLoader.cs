6972ff4e92882dfc8ad9dfee0b409cfbc7575b31
TMalec96 <tomasz.malecki1996@gmail.com>
2020-02-16 20:37:54 +0100

    Screens, game over condition

---
+++ EndlessTavern/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+   public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadGameScene()
+    {
+        FindObjectOfType<GameSession>().ResetGame();
+        SceneManager.LoadScene(1);
+      
+
+    }
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
