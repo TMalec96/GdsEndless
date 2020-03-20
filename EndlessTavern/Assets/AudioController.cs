cfbdbdb528d444fa502943ea1e09290387a82403
TMalec96 <tomasz.malecki1996@gmail.com>
2020-03-20 13:59:52 +0100

    Settings working

---
+++ EndlessTavern/Assets/AudioController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField]
+    AudioMixer masterMixer;
+    private bool isMusicOn = true;
+    public bool IsMusicOn
+    {
+        get { return isMusicOn; }
+        set { isMusicOn = value; }
+    }
+    private bool isSfxOn = true;
+    public bool IsSfxOn
+    {
+        get { return isSfxOn; }
+        set { isSfxOn = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpSingleton();
+    }
+    private void SetUpSingleton()
+    {
+        int numberAudioControllers = FindObjectsOfType<AudioController>().Length;
+        if (numberAudioControllers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+   public void TurnOnMusic()
+    {
+            masterMixer.SetFloat("MusicVolume", 0);
+            isMusicOn = true;
+       
+ 
+    }
+    public void TurnOnSFX()
+    {
+            masterMixer.SetFloat("SfxVolume", 0);
+            isSfxOn = true;
+      
+    }
+    public void TurnOffSFX()
+    {
+        masterMixer.SetFloat("SfxVolume", -80);
+        isSfxOn = false;
+
+    }
+    public void TurnOffMusic()
+    {
+        masterMixer.SetFloat("MusicVolume",-80);
+        isMusicOn = false;
+
+    }
+
+
+}
--- EndlessTavern/Assets/Scripts/SettingsMenu.cs
+++ EndlessTavern/Assets/Scripts/SettingsMenu.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    Toggle musicToggle;
+    Toggle sfxToggle;
+    private void Start()
+    {
+        InitializeToggles();
+    }
     public void ActivateSettingsMenu()
     {
         gameObject.SetActive(true);
@@ -12,4 +19,47 @@ public class SettingsMenu : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+    private void InitializeToggles()
+    {
+        print("togglesInit");
+        musicToggle = transform.Find("MusicToggle").gameObject.GetComponent<Toggle>();
+        sfxToggle = transform.Find("SFXToggle").gameObject.GetComponent<Toggle>();
+        musicToggle.isOn = UpdateMusicToggleStatus();
+        sfxToggle.isOn = UpdateSFXMusicToggleStatus();
+        
+    }
+    public bool UpdateMusicToggleStatus()
+    {
+       return FindObjectOfType<AudioController>().IsMusicOn;
+    }
+    public bool UpdateSFXMusicToggleStatus()
+    {
+        return FindObjectOfType<AudioController>().IsSfxOn;
+
+    }
+    public void SwitchMusic()
+    {
+        
+            if (UpdateMusicToggleStatus() && UpdateMusicToggleStatus() != musicToggle.isOn)
+            {
+                FindObjectOfType<AudioController>().TurnOffMusic();
+            }
+            else if(!UpdateMusicToggleStatus() && UpdateMusicToggleStatus() != musicToggle.isOn)
+            {
+                FindObjectOfType<AudioController>().TurnOnMusic();
+            }
+        
+    }
+    public void SwitchSFX()
+    {
+        if (UpdateSFXMusicToggleStatus() && UpdateSFXMusicToggleStatus() != sfxToggle.isOn)
+        {
+            FindObjectOfType<AudioController>().TurnOffSFX();
+        }
+        else if (!UpdateSFXMusicToggleStatus() && UpdateSFXMusicToggleStatus() != sfxToggle.isOn)
+        {
+            FindObjectOfType<AudioController>().TurnOnSFX();
+        }
+    }
+    
 }
